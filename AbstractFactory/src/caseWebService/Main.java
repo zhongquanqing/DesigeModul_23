350f072b389adfde2dfcfdc5e55a6c3b5c233fcb
zhongquanqing <1053435813@qq.com>
2019-01-05 12:41:50 +0800

    提交

--- AbstractFactory/src/caseWebService/Main.java
+++ AbstractFactory/src/caseWebService/Main.java
@@ -10,11 +10,11 @@ public class Main {
     public static void main(String[] args) {
 
         AbstractColorFactory abstractColorFactory =new AbstractColorFactory();
+        //这里我只实现颜色工厂
         ColorFactory colorFactory=abstractColorFactory.colorFactory("Clolor");
         colorFactory.getColor("red").showValue();
         colorFactory.getColor("greed").showValue();
-		System.out.print("我第二次进来喔");
-		System.out.print("我第一次进来喔");
-		System.out.print("我第三次进来喔");
+        //有兴趣的童鞋可以实现一下图形工厂shapFactory
+
     }
 }
