adee5993279caa1846b637cd05d0de98fb795f0b
zhongquanqing <1053435813@qq.com>
2019-01-05 17:00:21 +0800

    提交

--- AbstractFactory/src/caseWebService/Main.java
+++ AbstractFactory/src/caseWebService/Main.java
@@ -6,11 +6,15 @@ import caseImplement.AbstractFactoryProducer;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        *        抽象工厂
+        * */
+
         //构造器（用于获取工厂）
         AbstractFactoryProducer abstractFactoryProducer =new AbstractFactoryProducer();
         //根据参数type获取对应的颜色工厂
         AbstractFactory abstractFactory=abstractFactoryProducer.getFactory("shape");
-        //根据参数color获取颜色
+        //根据参数color获取颜色类
         abstractFactory.getColor("red").showValue();
         abstractFactory.getColor("greed").showValue();
         System.out.println("gg"+abstractFactory.getClass());
